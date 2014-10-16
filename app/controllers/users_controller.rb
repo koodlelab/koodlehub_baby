3d33ee1ebc478846180af7fe2224bc69cc001d24
koodle noodle <jingchen98@yahoo.com>
2014-10-16 18:32:53 +0800

    add user posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,16 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
 
   def new
-    @user = User.new
+    if signed_in?
+      redirect_to(root_url)
+    else
+      @user = User.new
+    end
   end
+
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def index
@@ -16,14 +22,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      # Handle a successful save.
-      sign_in @user
-      flash[:success] = "Welcome to the Koodle Hub!"
-      redirect_to @user
+    if signed_in?
+      redirect_to(root_url)
     else
-      render 'new'
+      @user = User.new(user_params)
+      if @user.save
+        # Handle a successful save.
+        sign_in @user
+        flash[:success] = "Welcome to the Koodle Hub!"
+        redirect_to @user
+      else
+        render 'new'
+      end
     end
   end
 
@@ -43,8 +53,13 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User deleted."
+    @user = User.find(params[:id])
+    if current_user?(@user)
+      flash[:fail] = "Can not delete self."
+    else
+      @user.destroy
+      flash[:success] = "User deleted."
+    end
     redirect_to users_url
   end
 
@@ -55,12 +70,7 @@ class UsersController < ApplicationController
                                  :password_confirmation)
   end
 
-  def signed_in_user
-    unless signed_in?
-      store_location
-      redirect_to signin_url, notice: "Please sign in."
-    end
-  end
+
 
   def correct_user
     @user = User.find(params[:id])
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
+
   # before_save { self.email = email.downcase }
   before_save { email.downcase! }
   before_create :create_remember_token
@@ -12,12 +14,18 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, length: {minimum: 6}
 
+
   def User.new_remember_token
     SecureRandom.urlsafe_base64
   end
 
   def User.digest(token)
     Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
   end
 
   private
